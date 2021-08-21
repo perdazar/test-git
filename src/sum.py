f39109c5593cba48bee911a42d750cf126ba2485
mario melendrez <perdazar@gmail.com>
2021-08-21 15:28:35 -0700

    sum

---
+++ src/sum.py
@@ -0,0 +1,15 @@
+
+[3~import sys 
+
+def my_sum(a, b): 
+
+           print(int(a) + int(b))
+
+my_sum(sys.argv[1], sys.argv[2])
+[D[3~[A[A[A[A[A[A[A[A[A[B[B[A[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[B[B[B[B[B[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[A[A[A[A[[B[B[B[C[C[C[C[C[C[C[C[C[[A[[B[C[C[C[C[C[C[C[C[C[A[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~ import sys 
+
+def my_sum(a, b): 
+
+           print(int(a) + int(b))
+
+my_sum(sys.argv[1], sys.argv[2])
